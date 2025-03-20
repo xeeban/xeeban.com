8bfd17efc106afb436e6bb917467fcccc8d01c8b
Nori Nishigaya <anishi@gmail.com>
2025-03-20 11:18:31 -0700

    Update Astro configuration and add .gitignore

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import githubPages from '@astrojs/github-pages';
 
 export default defineConfig({
   output: 'static',
   site: 'https://xeeban.github.io',
   base: '/xeeban.com',
-  integrations: [tailwind(), githubPages()],
+  integrations: [tailwind()]
 }); 
